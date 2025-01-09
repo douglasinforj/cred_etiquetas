a94199ffe545ad5d0716b6dd69f28eeb3829436a
Douglas Silva <douglasitpro@gmail.com>
2025-01-09 08:43:58 -0300

    Alinhando pastas do projeto

--- frontend/src/redux/stores.js
+++
@@ -1,9 +0,0 @@
-import  { configureStore } from '@reduxjs/toolkit';
-import participantesReducer from './reducers/participantesReducer'
-
-
-export const store = configureStore({
-    reducer: {
-        participantes: participantesReducer,
-    }
-})
